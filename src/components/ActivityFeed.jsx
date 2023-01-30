2eef99ddafd61bd5dd2c8e9889ac5053cf029402
Nicholas <nc_chau@hotmail.com>
2023-01-30 17:18:09 -0500

    ActivityFeed populated with api call

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+//components
 import Call from "./Call";
 
+//styles
 import "../css/activity-feed.css";
 
 const ActivityFeed = () => {
+  const [calls, setCalls] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities")
+    .then(response => {
+      setCalls(response.data);
+    })
+  }, []);
+
+  const callList = calls.map(call => {
+    return (
+      <Call
+      key={call.id}
+      id={call.id} 
+      from={call.from}
+      to={call.to}
+      direction={call.direction}
+      via={call.via}
+      duration={call.via}
+      call_type={call.call_type}
+      />
+    )
+  })
+
   return (
     <ul>
-      <Call />
-      <Call />
-      <Call />
+      {callList}
     </ul>
   )
 }
--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 import "../css/call.css";
 
-const Call = () => {
+const Call = (props) => {
   return (
     <li className="call-container">
       <span className="call-overview">
         <div className="call-overview-left">
           <div className="call-type">
-            <i class="fa-solid fa-phone"></i>
+            <i className="fa-solid fa-phone"></i>
           </div>
           <div>
-            <div>123 v333 4123</div>
-            <div>missed call from</div>
+            <div>{props.from}</div>
+            <div>missed call from {props.via}</div>
           </div>
         </div>
         <div className="call-overview-right">
